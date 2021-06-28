4b78a71f6b6c92895df16853e6aec99d8d971636
Montes Carballo Said Nahum <70162272+saidnahum@users.noreply.github.com>
2021-06-28 17:59:45 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Navbar/>
+  <div class="container">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+export default {
+  components:{
+    Navbar
+  }
+}
+</script>
